959df94f7c73e49d3747c1f3d0d23c96c577f8f6
Kirill <kirill.o.borisov@gmail.com>
2021-02-22 18:33:53 +0300

    My fifth commit

--- homework.py
+++ homework.py
@@ -51,7 +51,8 @@ class CashCalculator(Calculator):
             return f'На сегодня осталось {money_left} {money_name}'
         elif money_left == 0:
             return 'Денег нет, держись'
-        return f'Денег нет, держись: твой долг - {abs(money_left)} {money_name}'
+        return (f'Денег нет, держись: '
+                f'твой долг - {abs(money_left)} {money_name}')
 
 
 class CaloriesCalculator(Calculator):
